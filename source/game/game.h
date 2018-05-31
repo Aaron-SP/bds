c5c63c11db182c068efa74cb182686e7426fff20
Aaron-SP <springstrohaa@gmail.com>
2018-05-31 10:16:44 -0400

    BDS: Integrated ATI fix

--- source/game/game.h
+++ source/game/game.h
@@ -110,7 +110,7 @@ class bds
         _ui.text().set_debug_title("Beyond Dying Skies: Official Demo");
         _ui.text().set_debug_vendor(vendor);
         _ui.text().set_debug_renderer(render);
-        _ui.text().set_debug_version("VERSION: 0.1.246");
+        _ui.text().set_debug_version("VERSION: 0.1.247");
 
         // Set the game mode
         const bool hardcore = _state.get_load_state().is_hardcore();
